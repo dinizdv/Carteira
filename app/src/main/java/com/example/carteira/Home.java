2bb59f7cfcfb828cd2c5ae65b88b7eb984185296
dinizdv <dinizdev3@gmail.com>
2024-03-09 15:46:13 -0300

    feat(login): interface inicial da tela de login.

--- app/src/main/java/com/example/carteira/Home.java
+++ app/src/main/java/com/example/carteira/Home.java
@@ -3,11 +3,14 @@ package com.example.carteira;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.w3c.dom.Text;
+
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
