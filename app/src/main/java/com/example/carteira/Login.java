fd3db6cbd4a04aaf7a32c7ccb9f79949fbf78e4f
dinizdv <dinizdev3@gmail.com>
2024-03-24 14:37:13 -0300

    feat(Carteira): finalizando protótipo 1 da tela principal, com header, carteirinha, qr-code e ícones.

--- app/src/main/java/com/example/carteira/Login.java
+++ app/src/main/java/com/example/carteira/Login.java
@@ -1,11 +1,13 @@
 package com.example.carteira;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.carteira.controllers.LoginController;
 
 public class Login extends AppCompatActivity {
@@ -39,6 +41,8 @@ public class Login extends AppCompatActivity {
 
                 loginController.loginUsuario(username, password);
 
+                Intent intent = new Intent(Login.this, Initial.class);
+                startActivity(intent);
             }
         });
     }
